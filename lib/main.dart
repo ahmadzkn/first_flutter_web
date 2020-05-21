c457abeef96249da04d6ee8926050e0d33ffdc12
Ahmad Khan <ahmadzkn@gmail.com>
2020-05-21 11:32:22 +0500

    Changed my name to Ahmad Khan on text

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello Ahmad, You have pushed the button this many times:',
+              'Hello Ahmad Khan, You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
